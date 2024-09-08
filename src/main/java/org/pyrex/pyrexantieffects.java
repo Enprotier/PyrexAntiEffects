70f73f0e7a608655d8dff53de4aa476c43a4e4d9
Pyrex <126282272+PyrexPacks@users.noreply.github.com>
2024-09-08 19:35:26 +0200

    Full recode, full mukodik

--- src/main/java/org/pyrex/pyrexantieffects.java
+++ src/main/java/org/pyrex/pyrexantieffects.java
@@ -1,3 +1,4 @@
+// tuturoal: https://www.youtube.com/watch?v=dQw4w9WgXcQ
 package org.pyrex;
 
 import org.bukkit.Bukkit;
@@ -6,120 +7,83 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionData;
 import org.bukkit.potion.PotionEffectType;
 
 public class PyrexAntiEffects extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        Bukkit.getServer().getPluginManager().registerEvents(this, this);
-        getLogger().info("PyrexAntiEffects has been enabled.");
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getLogger().info("PyrexAntiEffects plugin elindult!");
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                PotiLopas(player.getInventory().getContents());
+            }
+        }, 0L, 20L);
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("PyrexAntiEffects has been disabled.");
+        getLogger().info("PyrexAntiEffects plugin elsullyed, mint a titanik");
     }
 
-    // Scan player's inventory on interaction
     @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        Player player = event.getPlayer();
-        scanAndRemoveItems(player);
+    public void EzeketAtLehetNevezniKerdojel(InventoryClickEvent event) {
+        PotiLopas(event.getWhoClicked().getInventory().getContents());
     }
 
-    // Scan player's inventory on item consume
     @EventHandler
-    public void onItemConsume(PlayerItemConsumeEvent event) {
-        Player player = event.getPlayer();
-        scanAndRemoveItems(player);
+    public void NemTudomDeAzIDENemKotekszik(InventoryDragEvent event) {
+        PotiLopas(event.getWhoClicked().getInventory().getContents());
     }
 
-    // Scan player's inventory on inventory click
     @EventHandler
-    public void onInventoryClick(InventoryClickEvent event) {
-        if (event.getWhoClicked() instanceof Player) {
-            Player player = (Player) event.getWhoClicked();
-            scanAndRemoveItems(player);
+    public void AMIKORCSATLAKOZIKEGYEMBER(PlayerJoinEvent event) {
+        PotiLopas(event.getPlayer().getInventory().getContents());
+    }
+
+    @EventHandler
+    public void YouDroppedUrSoapMan(PlayerDropItemEvent event) {
+        ItemStack droppedItem = event.getItemDrop().getItemStack();
+        if (MilyenPotiKerdojel(droppedItem)) {
+            event.getItemDrop().remove();
         }
     }
 
-    // Scan and remove potions/arrows with specific effects from inventory
-    private void scanAndRemoveItems(Player player) {
-        // Iterate through the player's entire inventory
-        for (ItemStack item : player.getInventory().getContents()) {
-            if (item != null && isSlownessOrSlowFallingItem(item, player)) {
-                player.getInventory().remove(item);
+    private void PotiLopas(ItemStack[] items) {
+        for (ItemStack item : items) {
+            if (item == null) continue;
+
+            if (MilyenPotiKerdojel(item)) {
+                item.setAmount(0);
             }
         }
-
-        // Also check offhand and armor slots (in case of future expansions)
-        ItemStack offhand = player.getInventory().getItemInOffHand();
-        if (offhand != null && isSlownessOrSlowFallingItem(offhand, player)) {
-            player.getInventory().remove(offhand);
-        }
     }
 
-    // Check if the item is a potion or arrow with slowness, slow falling, or turtle master effects
-    private boolean isSlownessOrSlowFallingItem(ItemStack item, Player player) {
-        // Handle potions (including Lingering, Splash, and Normal Potions)
+    private boolean MilyenPotiKerdojel(ItemStack item) {
         if (item.getType() == Material.POTION || item.getType() == Material.SPLASH_POTION || item.getType() == Material.LINGERING_POTION) {
-            if (item.getItemMeta() instanceof PotionMeta) {
-                PotionMeta meta = (PotionMeta) item.getItemMeta();
-
-                for (PotionEffect effect : meta.getCustomEffects()) {
-                    if (isRelevantEffect(effect)) {
-                        sendDebugMessage(player, item, effect);
-                        return true;
-                    }
-                }
-
-                // Check for base potion types like Turtle Master
-                if (meta.getBasePotionData().getType().toString().contains("TURTLE_MASTER")) {
-                    sendDebugMessage(player, item, null); // No specific custom effect
-                    return true;
-                }
+            PotionMeta potionMeta = (PotionMeta) item.getItemMeta();
+            if (potionMeta != null) {
+                PotionData potionData = potionMeta.getBasePotionData();
+                return potionData.getType().getEffectType() == PotionEffectType.SLOW_FALLING || potionData.getType().getEffectType() == PotionEffectType.SLOW;
             }
         }
 
-        // Handle tipped arrows with slowness or slow falling effects
         if (item.getType() == Material.TIPPED_ARROW) {
-            if (item.getItemMeta() instanceof PotionMeta) {
-                PotionMeta meta = (PotionMeta) item.getItemMeta();
-                for (PotionEffect effect : meta.getCustomEffects()) {
-                    if (isRelevantEffect(effect)) {
-                        sendDebugMessage(player, item, effect);
-                        return true;
-                    }
-                }
+            PotionMeta arrowMeta = (PotionMeta) item.getItemMeta();
+            if (arrowMeta != null) {
+                return arrowMeta.getBasePotionData().getType().getEffectType() == PotionEffectType.SLOW_FALLING ||
+                        arrowMeta.getBasePotionData().getType().getEffectType() == PotionEffectType.SLOW;
             }
         }
-
         return false;
-    }
-
-    // Determine if the potion effect is slowness or slow falling
-    private boolean isRelevantEffect(PotionEffect effect) {
-        return effect.getType() == PotionEffectType.SLOW || effect.getType() == PotionEffectType.SLOW_FALLING;
-    }
-
-    // Send debug message about removed items and effects
-    private void sendDebugMessage(Player player, ItemStack item, PotionEffect effect) {
-        String itemType = item.getType().toString();
-        if (effect != null) {
-            getLogger().info("Removed item: " + itemType + " from " + player.getName() + " with effect: " + effect.getType().getName()
-                    + " for " + effect.getDuration() + " ticks.");
-            player.sendMessage("Removed: " + itemType + " with effect: " + effect.getType().getName()
-                    + " for " + effect.getDuration() + " ticks.");
-        } else {
-            getLogger().info("Removed item: " + itemType + " from " + player.getName() + " (Turtle Master or base potion)");
-            player.sendMessage("Removed: " + itemType + " (Turtle Master or base potion)");
-        }
     }
 }
